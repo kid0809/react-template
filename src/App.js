4442602df35da10e12d4d088b251180d4b9d37b8
蓝熙 <xilan1@caih.com>
2020-06-30 08:34:35 +0800

    修改错误

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { HashRouter as Router } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 import Routes from './routes';
 import './styles/main.scss';
-
 class App extends Component {
     render() {
         return (
